55c9b499c68c362771c2cdeb9456b5febe6d719e
Mike Zhou <leshui.zhou@gmail.com>
2016-12-04 19:35:59 -0500

    Basic Website Template

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
