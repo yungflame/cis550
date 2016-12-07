e5a6b9bd40a26ec4de7eda5b550b5330ad614bfe
Tang Song <tangsong15@gmail.com>
2016-12-06 21:04:35 -0500

    tang

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+
 var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
-
+var index = require('./routes/index');
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
 
+router.get("/",index.do_work);
+
+/*
 router.get("/",function(req,res){
   res.sendFile(path + "index.html");
-});
+});*/
 
 router.get("/about",function(req,res){
   res.sendFile(path + "about.html");
@@ -29,3 +33,4 @@ app.use("*",function(req,res){
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
+*/
