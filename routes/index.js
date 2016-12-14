80765ea834ff8404ba23d55d102773ec9c21ffea
Max <maxying@sas.upenn.edu>
2016-12-13 21:27:52 -0500

    another query, and making code look nicer

--- routes/index.js
+++ routes/index.js
@@ -16,27 +16,35 @@ var connection;
 
 var query_set = [
 // Query 0
-	"SELECT A.GENDER, CI.MEDAL, COUNT(CI.MEDAL) AS NUM \
-	FROM COMPETES_IN CI INNER JOIN ATHLETE A \
-	  ON CI.NAME=A.NAME \
+	"SELECT CI.AGENDER, CI.MEDAL, COUNT(CI.MEDAL) AS NUM \
+	FROM COMPETES_IN CI \
 	WHERE CI.YEAR='2008' \
-	  AND A.COUNTRY='USA' \
-	GROUP BY A.GENDER, CI.MEDAL \
-	ORDER BY CI.MEDAL, A.GENDER",
-// Query 
+	  AND CI.COUNTRY='United States' \
+	GROUP BY CI.AGENDER, CI.MEDAL;",
+// Query 1
 	"SELECT C.YEAR, C.GDP \
 	FROM COUNTRY_DATA C \
 	WHERE C.COUNTRY='China' \
-	  AND C.YEAR > 1990"
+	  AND C.YEAR > 1990;",
+// Query 2
+	"SELECT t1.MEDAL, t1.EGENDER, COUNT(t1.MEDAL) AS NUM \
+	FROM \
+	(SELECT DISTINCT CI.MEDAL, CI.EGENDER, CI.EVENT, CI.DISCIPLINE \
+	FROM COMPETES_IN CI \
+	WHERE CI.COUNTRY = 'United States' \
+	  AND CI.YEAR = 2008 \
+	ORDER BY CI.MEDAL, CI.EGENDER) t1 \
+	GROUP BY t1.MEDAL, t1.EGENDER;"
 ]
 
 var chart_description_set = [ 
 	"The medal distribution between US male and female medalists in the Beijing 2008 Olympics",
-	"Comparing the GDP of China to Russia over time"
+	"Comparing the GDP of China to Russia over time",
+	"The medal distribution of the US by event gender at the Beijing 2008 Olympics"
 ]
 
 function query_db(res, query_number) { 
-		connection.query(query_set[0] + '; ' + query_set[1],
+		connection.query(query_set[0] + query_set[1] + query_set[2],
 		function (error, row, fields) {
 			if (error) {
 				console.log(error); 
@@ -46,7 +54,6 @@ function query_db(res, query_number) {
 				output_homepage(res, row, query_number);
 			}
 		});
-	
 }
 
 function output_homepage(res,results, query_number) {
@@ -62,6 +69,6 @@ function output_homepage(res,results, query_number) {
  
 exports.do_work = function(req, res){
 	connection = mysql.createConnection(db_config);
-	var i = Math.floor((Math.random()+1)); // RNG FOR Homepage Chart
+	var i = Math.floor(query_set.length * Math.random()); // RNG FOR Homepage Chart
 	query_db(res, i);
 };
