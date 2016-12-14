39755946251cb6cfa83b2e01c18688a5db827710
Mike Zhou <leshui.zhou@gmail.com>
2016-12-13 20:36:20 -0500

    working multiple charts pipeline

--- routes/index.js
+++ routes/index.js
@@ -8,38 +8,45 @@ var db_config ={
   host: 'cis550group33.chgqikrkerri.us-west-2.rds.amazonaws.com',
   user: 'maxying',
   password: 'cis550group33',
-  database: 'PENNTR'
+  database: 'PENNTR',
+  multipleStatements: true
 };
 
 var connection;
 
 var query_set = [
+// Query 0
 	"SELECT A.GENDER, CI.MEDAL, COUNT(CI.MEDAL) AS NUM \
 	FROM COMPETES_IN CI INNER JOIN ATHLETE A \
 	  ON CI.NAME=A.NAME \
 	WHERE CI.YEAR='2008' \
 	  AND A.COUNTRY='USA' \
 	GROUP BY A.GENDER, CI.MEDAL \
-	ORDER BY CI.MEDAL, A.GENDER;"
+	ORDER BY CI.MEDAL, A.GENDER",
+// Query 
+	"SELECT C.YEAR, C.GDP \
+	FROM COUNTRY_DATA C \
+	WHERE C.COUNTRY='China' \
+	  AND C.YEAR > 1990"
 ]
 
 var chart_description_set = [ 
-	"The medal distribution between US male and female medalists in the Beijing 2008 Olympics"
+	"The medal distribution between US male and female medalists in the Beijing 2008 Olympics",
+	"Comparing the GDP of China to Russia over time"
 ]
 
-function query_db(res, query, query_number) {
-	connection.query({
-	sql: query,
-	timeout: 3000, // 3s
-	}, function (error, results, fields) {
-		if (error) {
-			console.log(error); 
-		}
-		else {
-			output_homepage(res, results, query_number);
-			console.log(results)
-		}
-	});
+function query_db(res, query_number) { 
+		connection.query(query_set[0] + '; ' + query_set[1],
+		function (error, row, fields) {
+			if (error) {
+				console.log(error); 
+			}
+			else {
+				console.log(row);
+				output_homepage(res, row, query_number);
+			}
+		});
+	
 }
 
 function output_homepage(res,results, query_number) {
@@ -55,6 +62,6 @@ function output_homepage(res,results, query_number) {
  
 exports.do_work = function(req, res){
 	connection = mysql.createConnection(db_config);
-	var i = Math.floor((Math.random())); // always returns 0 for now
-	query_db(res, query_set[i], i);
+	var i = Math.floor((Math.random()+1)); // RNG FOR Homepage Chart
+	query_db(res, i);
 };
