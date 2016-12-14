74bf3e9b9d0b22a6f51ec4a3bda47cc4225b7e5a
Tang Song <tangsong15@gmail.com>
2016-12-13 23:11:06 -0500

    userform stuff

--- routes/userform.js
+++ routes/userform.js
@@ -1,3 +1,32 @@
+var mysql = require('mysql');
+
+var db_config ={
+  host: 'cis550group33.chgqikrkerri.us-west-2.rds.amazonaws.com',
+  user: 'maxying',
+  password: 'cis550group33',
+  database: 'PENNTR',
+  multipleStatements: true
+};
+
+var connection;
+
+function query_db(res, query_number) { 
+		var multiquery = '';
+		for (var i = 0; i < query_set.length; i++) {
+			multiquery += query_set[i];
+		}
+		connection.query(multiquery,
+		function (error, row, fields) {
+			if (error) {
+				console.log(error); 
+			}
+			else {
+				//console.log(row);
+				output_homepage(res, row, query_number);
+			}
+		});
+}
+
 exports.do_work = function(req, res){
   var country = req.query.country; // $_GET["country"]
   if (country) { 
