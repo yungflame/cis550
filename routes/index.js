b3595dee40e32b4c337fedbeede22579df706aed
Max <maxying@sas.upenn.edu>
2016-12-13 23:07:19 -0500

    another query

--- routes/index.js
+++ routes/index.js
@@ -34,17 +34,25 @@ var query_set = [
 	WHERE CI.COUNTRY = 'United States' \
 	  AND CI.YEAR = 2008 \
 	ORDER BY CI.MEDAL, CI.EGENDER) t1 \
-	GROUP BY t1.MEDAL, t1.EGENDER;"
+	GROUP BY t1.MEDAL, t1.EGENDER;",
+// Query 3
+	"SELECT H.COUNTRY, H.YEAR, H.COST, C.GDP \
+	FROM HOST_CITY H, COUNTRY_DATA C \
+	WHERE H.COUNTRY = C.COUNTRY \
+	  AND H.YEAR = C.YEAR \
+	  AND H.COST IS NOT NULL \
+	  AND H.YEAR >= 1972;"
 ]
 
 var chart_description_set = [ 
 	"The medal distribution between US male and female medalists in the Beijing 2008 Olympics",
 	"Comparing the GDP of China to Russia over time",
-	"The medal distribution of the US by event gender at the Beijing 2008 Olympics"
+	"The medal distribution of the US by event gender at the Beijing 2008 Olympics",
+	"Cost of the Olympics for the host country vs log-GDP (both in USD)"
 ]
 
 function query_db(res, query_number) { 
-		connection.query(query_set[0] + query_set[1] + query_set[2],
+		connection.query(query_set[0] + query_set[1] + query_set[2] + query_set[3],
 		function (error, row, fields) {
 			if (error) {
 				console.log(error); 
